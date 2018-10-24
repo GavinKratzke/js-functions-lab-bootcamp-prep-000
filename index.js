d741b31ae44920983c97c5b47f39a412125a3005
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-24 21:37:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function happyHolidayTo(holiday, name) {
  console.log('Happy ${holiday}, ${name}')
  return ('Happy Independence Day, you!')
 }
+function holidayCountdown(holiday, days) {
+  console.log('It\'s ${days} days until ${holiday}!')
+  return ("It's 5 days until Christmas!")
+}
